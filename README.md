e04a1cf082a8cbbcb6d28a7cd50fd36434c605bd
Patrik Hartwig <patrik.hartwig@megaport.com>
2015-06-15 10:38:15 +1000

    initial commit for github repo

--- README.md
+++ README.md
@@ -1,2 +1,138 @@
-# megaport.js
-megaport javascript api
+# Megaport JS Wrapper functions
+
+## Initilising the wrapper
+
+Staging Url 'https://api-staging.i.megaport.com'
+
+```javascript
+megaport = new mp('https://api-staging.i.megaport.com/v2/');
+```
+
+### Authenticating a User
+```javascript
+megaport.auth({
+  username: 'wsmithers',
+  password: 'password'
+});
+
+```
+If you have a valid token you can pass that instead, { token: 'tokenstring' }
+
+### Once Authenticated
+
+######megaport.ready(callback)
+
+Callback is run once a megaport wrapper has been authenticated.
+
+```javascript
+megaport.ready(
+  function(credentials) {
+    console.log(credentials);
+  }
+);
+```
+
+### On Authentication Failure
+
+###### megaport.failauth(callback)
+
+Callback is run once a megaport wrapper has failed an auth.
+
+```javascript
+megaport.failauth(function () {
+  console.log('Authentication Failed');
+});
+```
+
+## User Profile
+
+Returning current authenicated profile information
+
+```javascript
+megaport.profile().then(
+  function (profileObj) {
+    console.log(profileObj);
+  }
+);
+```
+
+
+## Managing Services (products)
+
+Return all services grouped by service groups you belong to.
+
+
+
+```javascript
+megaport.servicegroups().then(
+  function (servicegroups) {
+    console.log(servicegroups);
+  }
+);
+```
+
+Example Return
+
+```json
+[
+  {
+    serviceGroupId: 122,
+    megaports: [
+      {
+        productId: int,
+        associatedIxs: [...],
+        associatedVxcs: [...],
+        productName: "",
+        productType: "",
+        etc ...
+      }
+    ]
+  }
+]
+```
+
+### Get Service Details by productId
+
+megaport.product(int)
+
+works for both megaport and vxc and soon IX
+
+```javascript
+megaport.product(1).then(
+  function (product) {
+    console.log(product);
+  }
+);
+```
+
+### Update Service Details by productId
+
+megaport.product(int).update(obj);
+
+The object will have different params for product types (megaport/vxc/ix)
+
+```json
+megaport : {
+  "name": String,
+  "favourite": true/false
+}
+```
+
+```json
+vxc : {
+  "name": String,
+  "rateLimit": int,
+  "aVlan": int,
+  "bVlan": int
+}
+```
+
+```javascript
+megaport.product(728).update({
+  name: "SG test port" //SG test port
+}).then(
+  function (product) {
+    console.log(product);
+  }
+);
+```
---
+++ megaport.js
@@ -0,0 +1,556 @@
+/* jshint -W083 */
+var mp = (function () {
+  var exports = function (baseurl) {
+    var innerthis = this;
+    var onready, failauth, authUrl, authParams;
+    this.credentials = {};
+
+
+    var xhr = new xhreq();
+    q = new que();
+
+    this.auth = function (obj) {
+      if (typeof obj.username == 'string' && typeof obj.password == 'string') {
+        authUrl = baseurl + '/login';
+        authParams = {
+          username: obj.username,
+          password: obj.password
+        };
+      } else if (typeof obj.token == 'string') {
+        authUrl = baseurl + '/login/' + obj.token;
+        authParams = {};
+      } else if (typeof obj.access_token == 'string' && typeof obj.network == 'string') {
+        authUrl = baseurl + '/social/login/' + obj.network;
+        authParams = {
+          access_token: obj.access_token,
+          //redirect_uri: location.origin
+        };
+      }
+      //console.log(authParams,authUrl);
+      if (!authUrl || !authParams) return false;
+      xhr.post(authUrl, authParams).then(
+        function (d) {
+          innerthis.credentials = d.data;
+
+          if (typeof onready == 'function') {
+            onready(innerthis.credentials);
+
+            console.log('Login Success');
+          }
+
+          q.ready();
+        },
+        function (d) {
+          console.warn('Login Failed, constructor useless ' + d.status);
+          console.log(d);
+          if (d.status == 401)
+            if (typeof failauth == 'function')
+              failauth();
+        }
+      );
+    };
+
+    // /secure/dropdowns/locations
+    this.ready = function (cb) {
+      onready = cb;
+      return innerthis;
+    };
+
+    this.failauth = function (cb) {
+      failauth = cb;
+    };
+
+    this.destroy = function (cb) {
+      auth = {};
+      if (typeof cb == 'function')
+        cb(cb);
+    };
+
+
+    this.dashboard = function (ext) {
+      return new Promise(function (resolve, reject) {
+        q.onready(function () {
+          xhr.get(baseurl + '/dashboard' + (ext ? '/' + ext : ''), {}, innerthis.credentials.token)
+            .then(
+              function (d) {
+                resolve(d.data);
+              },
+              function (d) {
+                reject(d);
+                console.log(d);
+              }
+            );
+        });
+      });
+    };
+
+    this.menuStats = function (ext) {
+      return new Promise(function (resolve, reject) {
+        q.onready(function () {
+          xhr.get(baseurl + '/menuStats', {}, innerthis.credentials.token)
+            .then(
+              function (d) {
+                resolve(d.data);
+              },
+              function (d) {
+                reject(d);
+                console.log(d);
+              }
+            );
+        });
+      });
+    };
+
+    this.tickets = function (status) {
+      status = status || 'ANY';
+      return new Promise(function (resolve, reject) {
+        q.onready(function () {
+          xhr.get(baseurl + '/ticket', {
+              status: status
+            }, innerthis.credentials.token)
+            .then(
+              function (d) {
+                resolve(d.data);
+              },
+              function (d) {
+                reject(d);
+                console.log(d);
+              }
+            );
+        });
+      });
+    }
+
+
+    this.ixp = function (peerid, rsid, productid) {
+      var url = (peerid ? '/ixp/' + peerid + '/peers' : '/ixp');
+      if (peerid && rsid && productid)
+        url = '/ixp/' + peerid + '/' + rsid + '/product/' + productid + '/prefixes';
+      return new Promise(function (resolve, reject) {
+        q.onready(function () {
+          xhr.get(baseurl + url, {}, innerthis.credentials.token)
+            .then(
+              function (d) {
+                resolve(d.data);
+              },
+              function (d) {
+                reject(d);
+                console.log(d);
+              }
+            );
+        });
+      });
+    };
+
+    this.ixpGraph = function (productid, to, from) {
+      var url = '/graph';
+      var pObj = {
+        productId: productid,
+      };
+
+
+      if (!to)
+        pObj.to = new Date().getTime();
+      if (!from)
+        pObj.from = pObj.to - 86400000;
+
+      return new Promise(function (resolve, reject) {
+        q.onready(function () {
+          xhr.get(baseurl + url, pObj, innerthis.credentials.token)
+            .then(
+              function (d) {
+                resolve(d.data);
+              },
+              function (d) {
+                reject(d);
+                console.log(d);
+              }
+            );
+        });
+      });
+    };
+
+
+    this.servicegroups = function () {
+      return new Promise(function (resolve, reject) {
+        q.onready(function () {
+          xhr.get(baseurl + '/servicegroups', {}, innerthis.credentials.token)
+            .then(
+              function (d) {
+                resolve(d.data);
+              },
+              function (d) {
+                reject(d);
+                console.log(d);
+              }
+            );
+        });
+      });
+    };
+
+    this.product = function (productId) {
+      // /v2/dropdowns/locations
+      return new Promise(function (resolve, reject) {
+        q.onready(function () {
+          xhr.get(baseurl + '/product/' + productId, {}, innerthis.credentials.token)
+            .then(
+              function (d) {
+                resolve(d.data);
+              },
+              function (d) {
+                console.log(d);
+              }
+            );
+        });
+      });
+    };
+
+    this.locations = function () {
+      // /v2/dropdowns/locations
+
+      return new Promise(function (resolve, reject) {
+        q.onready(function () {
+          xhr.get(baseurl + '/dropdowns/locations', {}, innerthis.credentials.token)
+            .then(
+              function (d) {
+                resolve(d.data);
+              },
+              function (d) {
+                console.log(d);
+              }
+            );
+        });
+      });
+    };
+
+    this.market = function (id, obj) {
+      if (typeof id == 'object' && !obj) {
+        return new Promise(function (resolve, reject) {
+          q.onready(function () {
+            xhr.post(baseurl + '/market/' + id, obj, innerthis.credentials.token)
+              .then(
+                function (d) {
+                  resolve(d);
+                },
+                function (d) {
+                  reject(d);
+                }
+              );
+          });
+        });
+      }
+      if (typeof id == 'number' && typeof obj == 'object') {
+        return new Promise(function (resolve, reject) {
+          q.onready(function () {
+            xhr.put(baseurl + '/market/' + id, obj, innerthis.credentials.token)
+              .then(
+                function (d) {
+                  resolve(d);
+                },
+                function (d) {
+                  reject(d);
+                }
+              );
+          });
+        });
+      }
+      if (typeof id == 'number' && !obj) {
+        return new Promise(function (resolve, reject) {
+          q.onready(function () {
+            xhr.get(baseurl + '/market/' + id, {}, innerthis.credentials.token)
+              .then(
+                function (d) {
+                  resolve(d.data);
+                },
+                function (d) {
+                  reject(d);
+                }
+              );
+          });
+        });
+      }
+      return new Promise(function (resolve, reject) {
+        q.onready(function () {
+          xhr.get(baseurl + '/market', {}, innerthis.credentials.token)
+            .then(
+              function (d) {
+                resolve(d.data);
+              },
+              function (d) {
+                reject(d);
+              }
+            );
+        });
+      });
+    };
+
+    this.company = function (obj) {
+      if (obj) {
+        return new Promise(function (resolve, reject) {
+          q.onready(function () {
+            xhr.put(baseurl + '/company', obj, innerthis.credentials.token)
+              .then(
+                function (d) {
+                  resolve(d);
+                },
+                function (d) {
+                  reject(d);
+                }
+              );
+          });
+        });
+      }
+      return new Promise(function (resolve, reject) {
+        q.onready(function () {
+          xhr.get(baseurl + '/company', {}, innerthis.credentials.token)
+            .then(
+              function (d) {
+                resolve(d.data);
+              },
+              function (d) {
+                reject(d);
+              }
+            );
+        });
+      });
+    };
+
+    this.employment = function (obj) {
+      return new Promise(function (resolve, reject) {
+        q.onready(function () {
+          xhr.get(baseurl + '/employment', {}, innerthis.credentials.token)
+            .then(
+              function (d) {
+                resolve(d.data);
+              },
+              function (d) {
+                reject(d);
+              }
+            );
+        });
+      });
+    };
+
+    this.profile = function (obj) {
+
+      if (typeof obj == 'object') {
+        return new Promise(function (resolve, reject) {
+          q.onready(function () {
+            xhr.put(baseurl + '/employee/' + innerthis.credentials.personId, obj, innerthis.credentials.token)
+              .then(
+                function (d) {
+                  resolve(d);
+                },
+                function (d) {
+                  reject(d);
+                }
+              );
+          });
+        });
+      } else {
+        return new Promise(function (resolve, reject) {
+          q.onready(function () {
+            xhr.get(baseurl + '/employee/' + innerthis.credentials.personId, {}, innerthis.credentials.token)
+              .then(
+                function (d) {
+                  resolve(d.data);
+                },
+                function (d) {
+                  reject(d);
+                }
+              );
+          });
+        });
+      }
+
+    }
+
+
+    this.register = function (obj) {
+      // https://git.megaport.com/snippets/82
+
+      return new Promise(function (resolve, reject) {
+        xhr.post(baseurl + '/social/registration', obj)
+          .then(
+            function (d) {
+              resolve(d.data);
+            },
+            function (d) {
+              reject(d);
+            }
+          );
+      });
+    }
+  };
+
+
+
+  // que things that run prior to ready state;
+  function que(callback) {
+    var state = false;
+    var qued = [];
+
+    this.onready = function (callback) {
+      if (state === false)
+        qued.push(callback);
+      else
+        callback();
+    };
+
+    this.ready = function () {
+      state = true;
+      for (var f in qued) {
+        if (typeof qued[f] == 'function')
+          qued[f]();
+      }
+      qued = [];
+    };
+  }
+
+  // simple promise buider
+  function Promise(fn) {
+    var state = 'pending';
+    var value;
+    var deferred = null;
+
+    function resolve(newValue) {
+      if (newValue && typeof newValue.then === 'function') {
+        newValue.then(resolve, reject);
+        return;
+      }
+      state = 'resolved';
+      value = newValue;
+
+      if (deferred) {
+        handle(deferred);
+      }
+    }
+
+    function reject(reason) {
+      state = 'rejected';
+      value = reason;
+
+      if (deferred) {
+        handle(deferred);
+      }
+    }
+
+    function handle(handler) {
+      if (state === 'pending') {
+        deferred = handler;
+        return;
+      }
+
+      var handlerCallback;
+
+      if (state === 'resolved') {
+        handlerCallback = handler.onResolved;
+      } else {
+        handlerCallback = handler.onRejected;
+      }
+
+      if (!handlerCallback) {
+        if (state === 'resolved') {
+          handler.resolve(value);
+        } else {
+          handler.reject(value);
+        }
+
+        return;
+      }
+
+      var ret = handlerCallback(value);
+      handler.resolve(ret);
+    }
+
+    this.then = function (onResolved, onRejected) {
+      return new Promise(function (resolve, reject) {
+        handle({
+          onResolved: onResolved,
+          onRejected: onRejected,
+          resolve: resolve,
+          reject: reject
+        });
+      });
+    };
+
+    fn(resolve, reject);
+  }
+
+  var xhreq = function () {
+    var innerthis = this;
+
+    this.ajax = function (method, url, params, token) {
+      return new Promise(function (resolve, reject) {
+        method = method.toUpperCase();
+
+        if (typeof token == 'string')
+          url += '?token=' + token;
+
+        if (method == 'GET') {
+          if (typeof params == 'object') {
+            params = (function (obj) {
+              var str = [];
+              for (var p in obj)
+                if (obj.hasOwnProperty(p))
+                  str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+
+              return str.join("&");
+            })(params);
+            url += '&' + params;
+          }
+        }
+
+        var rq = new XMLHttpRequest();
+
+        rq.open(method, url, true);
+        rq.onload = function () {
+          if (rq.status == 200) {
+            resolve(JSON.parse(rq.responseText));
+          }
+          if (rq.status > 200) {
+            reject(JSON.parse(rq.responseText));
+          }
+          if (rq.status == 400) {
+
+          }
+        };
+        rq.onerror = function () {};
+        if (method == 'POST') {
+          rq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+          if (typeof params == 'object') {
+            params = (function (obj) {
+              var str = [];
+              for (var p in obj)
+                if (obj.hasOwnProperty(p))
+                  str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+
+              return str.join("&");
+            })(params);
+          }
+          rq.send(params);
+        } else if (method == 'PUT') {
+          rq.setRequestHeader('Content-Type', 'application/json');
+          rq.send(JSON.stringify(params));
+        } else {
+          rq.send();
+        }
+      });
+    };
+
+    this.get = function (url, params, token) {
+      return innerthis.ajax('GET', url, params, token);
+    };
+    this.post = function (url, params, token) {
+      return innerthis.ajax('POST', url, params, token);
+    };
+    this.put = function (url, params, token) {
+      return innerthis.ajax('PUT', url, params, token);
+    };
+  };
+
+  return exports;
+})();
+
+if (typeof module == 'object')
+  module.exports = mp;
