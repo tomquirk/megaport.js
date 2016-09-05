3300b52d61981c7af6647ab2f18bd042268dfe2b
Patrik Hartwig <patrik.hartwig@megaport.com>
2016-09-05 10:58:31 +1000

    test

--- megaport.js
+++ megaport.js
@@ -1540,7 +1540,8 @@ var mp = (function () {
                       resolve(d.data || d);
                     },
                     function (d) {
-                      reject(d);
+                      if (typeof reject == 'function')
+                        reject(d);
                       if (typeof errors == 'function')
                         errors(d);
                     }
